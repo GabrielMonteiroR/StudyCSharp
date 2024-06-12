44ecd0a7ef19dee37c1df331f7467b68e821bdd0
Gabriel <gmonteiro810@gmail.com>
2024-06-12 17:05:00 -0300

    Section 4 completed

---
+++ Section4/POO/POO/Models/Triangle.cs
@@ -0,0 +1,25 @@
+﻿namespace Poo.Models;
+
+public class Triangle
+{
+    private double _sideA;
+    private double _sideB;
+    private double _sideC;
+
+    public double SideA => _sideA;
+    public double SideB => _sideB;
+    public double SideC => _sideC;
+
+    public Triangle(double a, double b, double c)
+    {
+        _sideA = a;
+        _sideB = b;
+        _sideC = c;
+    }
+
+    public double Area()
+    {
+        double p = ((SideA + SideB + SideC) / 2);
+        return Math.Sqrt(p * ((p - SideA) * (p - SideB) + (p - SideC)));
+    }
+}
