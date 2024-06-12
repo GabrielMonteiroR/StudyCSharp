cdbef4264a64531bb2ad970cacaeb1ea1de2e9c2
Gabriel <gmonteiro810@gmail.com>
2024-06-12 15:25:03 -0300

    section 3

---
+++ Section3/ClassOne/ClassOne/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SByte x = 100;
+            long y = 231312312;
+
+            Console.WriteLine($"{x:f1} e {y:f2}");
+        }
+    }
+}
---
+++ Section3/DesafioOne/DesafioOne/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace chalenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void ShowProducts(string firstProduct, double priceOne, string secondProduct, double priceTwo)
+            {
+                Console.WriteLine($"{firstProduct} cujo preço é {priceOne}\n{secondProduct} cujo preço é {priceTwo}");
+            }
+
+            static void ShowPerson(int age, int id, char gender)
+            {
+                Console.WriteLine($"Registro: {age} anos de idade, código {id} e gênero {gender}");
+            }
+
+
+
+
+
+            string firstProduct = "PC";
+            string secondProduct = "Desk";
+
+            byte age = 30;
+            int id = 5290;
+            char gender = 'M';
+
+            double priceOne = 2100.0;
+            double priceTwo = 650.50;
+            double avg = 53.23321;
+
+            ShowProducts(firstProduct, priceOne, secondProduct, priceTwo);
+            ShowPerson(age, id, gender);
+        }
+
+
+
+    }
+}
